87274dddeb689e84fc5fe56903ea3e8f188b437d
Yuqi Wang <new.wang@outlook.com>
2023-06-19 15:43:19 +0800

    完成了收藏部分的样式增加了一个登录界面

---
+++ src/components/Login.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="backgroud">
+    <div id="logo">mysystem</div>
+    <div id="login">
+      <el-card class="box-card">
+        <div id="form">
+          <el-input v-model="username" placeholder="请输入用户名"></el-input>
+          <el-input
+            placeholder="请输入密码"
+            v-model="password"
+            show-password
+          ></el-input>
+          <el-button type="primary">登录</el-button>
+        </div>
+      </el-card>
+    </div>
+    <div id="clock">
+      <div id="time">{{ time }}</div>
+      <div id="data">{{ data }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { nextTick } from 'vue/types/umd'
+
+export default {
+  name: 'Backgroud',
+  data() {
+    return {
+      username: '',
+      password: '',
+      time: '',
+      data: '',
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      setInterval(this.update_clock, 1000)
+    })
+  },
+  methods: {
+    update_clock: function () {
+      var d = new Date()
+      var year = d.getFullYear()
+      if (year < 10) {
+        year = '0' + year
+      }
+      var month = d.getMonth()
+      if (month < 10) {
+        month = '0' + month
+      }
+      var day = d.getDate()
+      if (day < 10) {
+        day = '0' + day
+      }
+      var hour = d.getHours()
+      if (hour < 10) {
+        hour = '0' + hour
+      }
+      var minute = d.getMinutes()
+      if (minute < 10) {
+        minute = '0' + minute
+      }
+      this.time = hour + ':' + minute
+      this.data = year + '/' + month + '/' + day
+    },
+  },
+}
+</script>
+
+<style>
+#backgroud {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background-image: linear-gradient(90deg, cyan, purple);
+  background-size: 400%;
+  animation: myanimation 10s forwards;
+}
+@keyframes myanimation {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+#logo {
+  color: aliceblue;
+  font-size: 25px;
+  font-weight: 800px;
+  text-transform: uppercase;
+  position: absolute;
+  top: 15%;
+  left: 15%;
+}
+#login {
+  position: absolute;
+  top: 30%;
+  left: 38%;
+}
+#el-card {
+  width: 373px;
+  height: 210px;
+  background-color: rgba(255, 255, 255, 255);
+  border-radius: 15px;
+}
+#form {
+  line-height: 60px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+#clock {
+  color: aliceblue;
+  position: absolute;
+  top: 70%;
+  left: 15%;
+  text-align: left;
+}
+#time {
+  font-size: 100px;
+}
+#date {
+  font-size: 35px;
+}
+</style>
--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="Favorites">
     <Artworks></Artworks>
+    <ul class="Favorites-main">
+      <li v-for="image in images" :key="image.id">
+        <div>
+          <img :src="image.url" />
+        </div>
+        <span v-text="image.name"></span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,11 +20,152 @@ export default {
   components: {
     Artworks,
   },
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          name: '「おはよう」',
+          url: require('@/assets/images/picture/收藏/【FGO】征服王はいいぞ.png'),
+        },
+        {
+          id: 2,
+          name: '不定期画報2」',
+          url: require('@/assets/images/picture/收藏/不定期画報2.png'),
+        },
+        {
+          id: 3,
+          name: '129向こう側の月',
+          url: require('@/assets/images/picture/收藏/129向こう側の月.png'),
+        },
+        {
+          id: 4,
+          name: '東方絵まとめ11',
+          url: require('@/assets/images/picture/收藏/東方絵まとめ11.png'),
+        },
+        {
+          id: 5,
+          name: '髪の毛描くメモ',
+          url: require('@/assets/images/picture/收藏/髪の毛描くメモ.jpg'),
+        },
+        {
+          id: 6,
+          name: '風鎮め',
+          url: require('@/assets/images/picture/收藏/風鎮め.png'),
+        },
+        {
+          id: 7,
+          name: '六章',
+          url: require('@/assets/images/picture/收藏/六章.jpg'),
+        },
+        {
+          id: 8,
+          name: '魔理沙の遺言',
+          url: require('@/assets/images/picture/收藏/魔理沙の遺言.jpg'),
+        },
+        {
+          id: 9,
+          name: '水面の向こう',
+          url: require('@/assets/images/picture/收藏/水面の向こう.jpg'),
+        },
+        {
+          id: 10,
+          name: '无题',
+          url: require('@/assets/images/picture/收藏/无题.png'),
+        },
+        {
+          id: 11,
+          name: '移植都市と女学生',
+          url: require('@/assets/images/picture/收藏/移植都市と女学生.jpg'),
+        },
+        {
+          id: 12,
+          name: 'ARIA',
+          url: require('@/assets/images/picture/收藏/ARIA.jpg'),
+        },
+        {
+          id: 13,
+          name: 'Camelot and Happy New Year.',
+          url: require('@/assets/images/picture/收藏/Camelot and Happy New Year..jpg'),
+        },
+        {
+          id: 14,
+          name: 'FGOログⅣ',
+          url: require('@/assets/images/picture/收藏/FGOログⅣ.png'),
+        },
+        {
+          id: 15,
+          name: 'エレシュキガル',
+          url: require('@/assets/images/picture/收藏/エレシュキガル.jpg'),
+        },
+        {
+          id: 16,
+          name: 'シノブの世界',
+          url: require('@/assets/images/picture/收藏/シノブの世界.png'),
+        },
+        {
+          id: 17,
+          name: 'リヨぐだ子',
+          url: require('@/assets/images/picture/收藏/リヨぐだ子.png'),
+        },
+        {
+          id: 18,
+          name: 'ハグレ王国',
+          url: require('@/assets/images/picture/收藏/ハグレ王国.png'),
+        },
+        {
+          id: 19,
+          name: 'オルタ',
+          url: require('@/assets/images/picture/收藏/オルタ.png'),
+        },
+        {
+          id: 20,
+          name: '全て遠き理想郷',
+          url: require('@/assets/images/picture/收藏/全て遠き理想郷.png'),
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style scoped>
 #Favorites {
   padding: 0 10%;
+}
+
+/* 收藏主体 */
+.Favorites-main {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  margin: -5px;
+  height: 1200px;
+}
+
+.Favorites-main li {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* 列表每一项给出margin正值达到固定间隔 */
+  width: 250px;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.Favorites-main li div {
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.Favorites-main li img {
+  width: 100%;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+
+.Favorites-main img:hover {
+  opacity: 0.6;
+  transform: scale(1.5);
 }
 </style>
